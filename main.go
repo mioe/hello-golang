0a03bfc30055d89f13dc54ff888eda2f8f34507e
mioe <mioe.boss@gmail.com>
2021-05-06 15:04:31 +0300

    try net/http

--- main.go
+++ main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
+
+func hello_page(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("hello back!")
+	fmt.Fprintf(w, "hello front!")
+}
+
+func handleRequest() {
+	http.HandleFunc("/", hello_page)
+	http.HandleFunc("/foo/", hello_page)
+	http.ListenAndServe(":8080", nil)
+}
 
 func main() {
-	fmt.Println("hello world!")
+	handleRequest()
 }
